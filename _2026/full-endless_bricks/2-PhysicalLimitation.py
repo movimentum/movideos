8f5084f0b02f61f62e2604d4b430858b8304aff5
movimentum <movimentum.cave@gmail.com>
2026-02-04 00:20:26 +0300

    bricks(phys_limit): возможность смены ракурсов вращающейся камеры + удаление тестового кода

--- _2026/full-endless_bricks/2-PhysicalLimitation.py
+++ _2026/full-endless_bricks/2-PhysicalLimitation.py
@@ -19,169 +19,27 @@ SceneExtension.render_all_sections = False
 np.random.seed(0xDEADBEEF)
 
 
-#%%
-class PhysicalLimitation(ThreeDScene, SceneExtension):
-    
-    phi = PI / 2.5
-    theta = PI / 3
+#%% Расчёт наибольшей нагрузки
+class PhysicalLimitation(MovingCameraScene, SceneExtension):
     
     def construct(self):
-        
-        self.begin_ambient_camera_rotation()
-
-        #
-        ## Начало
-        #
-        self.next_section('3D-brick', skip_animations=SceneExtension.skip(False))
-        
-        # Оси
-        w = 3
-        dX = w / 2
-        axes = ThreeDAxes(
-            x_range=(-dX, dX, 1),
-            y_range=(-dX, dX, 1),
-            z_range=(-dX, dX, 1),
-            x_length=w,
-            y_length=w,
-            z_length=w
-        )
-        axes.set_opacity(1.0).set_color(YELLOW_A)
-        self.set_camera_orientation(phi=self.phi, theta=self.theta)
-        
-        self.play(Create(axes))
-        
-        self.wait(2)
-
-########################################
-class AnimatedBrickTest(ThreeDScene, SceneExtension):
-    def construct(self):
-        # Размеры кирпича
-        a, b, c = 4, 2, 1  # длина, ширина, высота
-        
-        # Создание кирпича
-        brick = Cube(
-            side_length=1,
-            fill_color=RED_E,
-            fill_opacity=0.9,
-            stroke_color=WHITE,
-            stroke_width=2
-        ).scale([a, b, c])
-        
-        # Создание текстовых меток
-        title = Text("Кирпич с размерами", font_size=36, color=YELLOW)
-        title.to_edge(UP)
-        
-        dimensions_text = Text(
-            f"Длина (a) = {a}\nШирина (b) = {b}\nВысота (c) = {c}",
-            font_size=24,
-            color=WHITE
-        )
-        dimensions_text.to_edge(DOWN)
-        
-        # Начальная настройка сцены
-        self.add_fixed_in_frame_mobjects(title, dimensions_text)
-        self.set_camera_orientation(phi=70 * DEGREES, theta=45 * DEGREES)
-        
-        # Анимация появления
-        self.play(
-            Write(title),
-            Create(brick),
-            Write(dimensions_text),
-            run_time=2
-        )
-        self.wait(1)
-        
-        # Создание линий размеров
-        # Длина (a)
-        length_start = brick.get_corner(DL + IN)
-        length_end = brick.get_corner(DR + IN)
-        length_line = DashedLine(length_start, length_end, color=YELLOW, stroke_width=2)
-        length_label = Text("a", color=YELLOW, font_size=28)
-        length_label.next_to(length_line.get_center(), DOWN + OUT, buff=0.2)
-        
-        # Ширина (b)
-        width_start = brick.get_corner(DR + IN)
-        width_end = brick.get_corner(DR + OUT)
-        width_line = DashedLine(width_start, width_end, color=GREEN, stroke_width=2)
-        width_label = Text("b", color=GREEN, font_size=28)
-        width_label.next_to(width_line.get_center(), RIGHT + OUT, buff=0.2)
-        
-        # Высота (c)
-        height_start = brick.get_corner(DL + IN)
-        height_end = brick.get_corner(UL + IN)
-        height_line = DashedLine(height_start, height_end, color=BLUE, stroke_width=2)
-        height_label = Text("c", color=BLUE, font_size=28)
-        height_label.next_to(height_line.get_center(), LEFT + OUT, buff=0.2)
-        
-        # Добавление размеров
-        self.play(
-            Create(length_line),
-            Write(length_label),
-            run_time=1
-        )
-        
-        self.play(
-            Create(width_line),
-            Write(width_label),
-            run_time=1
-        )
-        
-        self.play(
-            Create(height_line),
-            Write(height_label),
-            run_time=1
-        )
-        
-        self.wait(0.5)
-        
-        # Список ракурсов камеры (phi, theta, zoom)
-        camera_views = [
-            (90, -90, 1.3),   # Вид спереди
-            (0, 0, 1.3),      # Вид сверху
-            (90, 0, 1.3),     # Вид сбоку
-            (60, 45, 1.2),    # Изометрический вид 1
-            (120, -45, 1.3),  # Диагональный вид снизу
-        ]
-        
-        # Анимация смены ракурсов
-        for i, (phi, theta, zoom) in enumerate(camera_views):
-            self.move_camera(
-                phi=phi * DEGREES,
-                theta=theta * DEGREES,
-                zoom=zoom,
-                run_time=2,
-                frame_center=brick.get_center()
-            )
-            self.wait(0.5)  # Пауза 0.5 секунды между сменами
-        
-        # Финальный вид и исчезновение
-        self.move_camera(
-            phi=70 * DEGREES,
-            theta=45 * DEGREES,
-            zoom=1,
-            run_time=2
-        )
-        self.wait(1)
-        
-        # Исчезновение
-        self.play(
-            FadeOut(brick),
-            FadeOut(length_line),
-            FadeOut(width_line),
-            FadeOut(height_line),
-            FadeOut(length_label),
-            FadeOut(width_label),
-            FadeOut(height_label),
-            FadeOut(title),
-            FadeOut(dimensions_text),
-            run_time=1.5
-        )
+        pass
 
 
+#%% Кирпич с размерами
 class BrickBreak(ThreeDScene, SceneExtension):
     
-    dimensions    = 3, 2, 1  # длина, ширина, высота кирпича
+    dimensions = 3, 2, 1  # длина, ширина, высота кирпича
     n_parts = 4, 3, 2  # количество осколков по длине, ширине, высоте
+    
+    # Список ракурсов камеры (phi, theta, zoom)
+    camera_views = [
+        (90, -90, 1),   # Вид спереди
+        (0, 0, 1),      # Вид сверху
+        (90, 0, 1),     # Вид сбоку
+        (60, 45, 1),    # Изометрический вид 1
+        (120, -45, 1),  # Диагональный вид снизу
+    ]
     
     def construct(self):
         
@@ -369,7 +227,13 @@ class BrickBreak(ThreeDScene, SceneExtension):
             FadeIn(dimH, shift=LEFT),
             run_time=2
         )
-        self.wait(20)
+        self.wait(2)
+        
+        self.change_camera_view(0, wait=5)
+        self.change_camera_view(1, wait=5)
+        self.change_camera_view(2, wait=5)
+        self.change_camera_view(3, wait=5)
+        self.change_camera_view(4, wait=5)
 
         
         # Останавливаем вращение камеры
@@ -387,6 +251,40 @@ class BrickBreak(ThreeDScene, SceneExtension):
                      fill_opacity=fill_opacity,
                      stroke_color=edge_color,
                      stroke_width=edge_width)
+    
+    
+    def change_camera_view(self, i, run_time=2, wait=0.5):
+        
+        phi, theta, zoom = self.camera_views[i]
+        
+        self.move_camera(
+            phi=phi * DEGREES,
+            theta=theta * DEGREES,
+            zoom=zoom,
+            run_time=run_time,
+            #frame_center=brick.get_center()
+        )
+        
+        if wait > 0:
+            self.wait(wait)
+    
+    
+    def draw_axes(self):
+        """ Рисуем 3D оси """
+        w = 3
+        dX = w / 2
+        axes = ThreeDAxes(
+            x_range=(-dX, dX, 1),
+            y_range=(-dX, dX, 1),
+            z_range=(-dX, dX, 1),
+            x_length=w,
+            y_length=w,
+            z_length=w
+        )
+        axes.set_opacity(1.0).set_color(YELLOW_A)
+        
+        self.play(Create(axes))
+        self.wait(2)
 
 
 #%% Тестовый рендер
